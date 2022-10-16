565c96f49370dc8b4984ec4fed89cedab95b12e8
Moaad Fattali <moaad.fattali@ovrsea.com>
2022-10-16 17:50:12 +0200

    begin treating cases of previously alive cells

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -2,7 +2,7 @@
 import * as matchers from "jest-extended";
 expect.extend(matchers);
 
-import { computeNextGeneration, Grid, nextStatus } from "./index";
+import { allNeighbours, computeNextGeneration, Grid, nextStatus } from "./index";
 
 describe("Game of life", () => {
   describe("Acceptance tests", () => {
@@ -18,10 +18,11 @@ describe("Game of life", () => {
         { x: 1, y: 0 },
       ];
 
-      expect(computeNextGeneration(initialGrid)).toEqual(initialGrid);
+      expect(computeNextGeneration(initialGrid)).toIncludeSameMembers(initialGrid);
     });
 
-    it(`Should remove cells that are in underpopulation
+    it.skip(`Should remove cells that are in underpopulation
+    and create cells that should re-live
   Case: rectangle of 4 cells and one isolated neighbourless cell
    oo
    oo o
@@ -37,11 +38,12 @@ describe("Game of life", () => {
       const survivingCells: Grid = [
         { x: 0, y: 0 },
         { x: 1, y: 1 },
+        { x: 2, y: 0 },
         { x: 0, y: 1 },
         { x: 1, y: 0 },
       ];
 
-      expect(computeNextGeneration(initialGrid)).toEqual(survivingCells);
+      expect(computeNextGeneration(initialGrid)).toIncludeSameMembers(survivingCells);
     });
 
     it(`Should remove cells that are in underpopulation
@@ -65,7 +67,7 @@ describe("Game of life", () => {
         { x: 1, y: 0 },
       ];
 
-      expect(computeNextGeneration(initialGrid)).toEqual(survivingCells);
+      expect(computeNextGeneration(initialGrid)).toIncludeSameMembers(survivingCells);
     });
 
     it(`Should remove cells that are in underpopulation
@@ -81,7 +83,7 @@ describe("Game of life", () => {
 
       const survivingCells: Grid = [{ x: 1, y: 0 }];
 
-      expect(computeNextGeneration(initialGrid)).toEqual(survivingCells);
+      expect(computeNextGeneration(initialGrid)).toIncludeSameMembers(survivingCells);
     });
 
     it(`Should remove cells that are in overpopulation
@@ -105,36 +107,79 @@ describe("Game of life", () => {
         { x: 2, y: 1 },
       ];
 
-      expect(computeNextGeneration(initialGrid)).toEqual(survivingCells);
+      expect(computeNextGeneration(initialGrid)).toIncludeSameMembers(survivingCells);
     });
   });
 
   describe("Unit tests", () => {
-    it("Less than two neighbours: should die of underpopulation - case 0 neighbours", () => {
-      expect(nextStatus(0)).toEqual("dead");
+    g;
+    describe("next status of previously alive cells", () => {
+      it("Less than two neighbours: should die of underpopulation - case 0 neighbours", () => {
+        expect(nextStatus(0, "alive")).toEqual("dead");
+      });
+
+      it("Less than two neighbours: should die of underpopulation - case 1 neighbour", () => {
+        expect(nextStatus(1, "alive")).toEqual("dead");
+      });
+
+      it("Two or three neighours: should continue living", () => {
+        expect(nextStatus(2, "alive")).toEqual("alive");
+      });
+
+      it("Two or three neighours: should continue living", () => {
+        expect(nextStatus(3, "alive")).toEqual("alive");
+      });
+
+      it("Two or three neighours: should die of over", () => {
+        expect(nextStatus(4, "alive")).toEqual("dead");
+      });
+      it("Two or three neighours: should continue living", () => {
+        expect(nextStatus(5, "alive")).toEqual("dead");
+      });
+
+      it("Two or three neighours: should continue living", () => {
+        expect(nextStatus(6, "alive")).toEqual("dead");
+      });
     });
 
-    it("Less than two neighbours: should die of underpopulation - case 1 neighbour", () => {
-      expect(nextStatus(1)).toEqual("dead");
+    describe("next status of previously dead cells", () => {
+      it("Any dead cell with not exactly three live neighbours stays dead", () => {
+        expect(nextStatus(2, "dead")).toEqual("dead");
+      });
+
+      it("Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction", () => {
+        expect(nextStatus(3, "dead")).toEqual("alive");
+      });
+
+      it("Control - Any dead cell with not exactly three live neighbours stays dead, as if by reproduction - case 2", () => {
+        expect(nextStatus(4, "dead")).toEqual("dead");
+      });
     });
 
-    it("Two or three neighours: should continue living", () => {
-      expect(nextStatus(2)).toEqual("alive");
-    });
+    describe("generating test of all neighbours of a cell", () => {
+      it("Generates list of all 8 neighbours of a given cell", () => {
+        const topRight = { x: 9, y: 11 };
+        const topCenter = { x: 8, y: 11 };
+        const topLeft = { x: 7, y: 11 };
 
-    it("Two or three neighours: should continue living", () => {
-      expect(nextStatus(3)).toEqual("alive");
-    });
+        const mediumRight = { x: 9, y: 10 };
+        const mediumLeft = { x: 7, y: 10 };
 
-    it("Two or three neighours: should die of over", () => {
-      expect(nextStatus(4)).toEqual("dead");
-    });
-    it("Two or three neighours: should continue living", () => {
-      expect(nextStatus(5)).toEqual("dead");
-    });
+        const bottomRight = { x: 9, y: 9 };
+        const bottomCenter = { x: 8, y: 9 };
+        const bottomLeft = { x: 7, y: 9 };
 
-    it("Two or three neighours: should continue living", () => {
-      expect(nextStatus(6)).toEqual("dead");
+        expect(allNeighbours({ x: 8, y: 10 })).toIncludeSameMembers([
+          topRight,
+          topCenter,
+          topLeft,
+          mediumRight,
+          mediumLeft,
+          bottomRight,
+          bottomCenter,
+          bottomLeft,
+        ]);
+      });
     });
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,27 @@ type AliveCell = Readonly<{
 
 export type Grid = AliveCell[];
 
-const isSameCell = ({ x: x1, y: y1 }: AliveCell, { x: x2, y: y2 }: AliveCell): boolean => x1 === x2 && y1 === y2;
+const isSameCell =
+  ({ x: x1, y: y1 }: AliveCell) =>
+  ({ x: x2, y: y2 }: AliveCell): boolean =>
+    x1 === x2 && y1 === y2;
 
 const areNeighbours = ({ x: x1, y: y1 }: AliveCell, { x: x2, y: y2 }: AliveCell): boolean =>
-  Math.abs(x1 - x2) <= 1 && Math.abs(y1 - y2) <= 1 && !isSameCell({ x: x1, y: y1 }, { x: x2, y: y2 });
+  Math.abs(x1 - x2) <= 1 && Math.abs(y1 - y2) <= 1 && !isSameCell({ x: x1, y: y1 })({ x: x2, y: y2 });
 
-const extractNeighbours = (currentCell: AliveCell, grid: Grid): AliveCell[] =>
+export const allNeighbours = ({ x: xCell, y: yCell }: AliveCell): AliveCell[] =>
+  [
+    { x: -1, y: 1 },
+    { x: 0, y: 1 },
+    { x: 1, y: 1 },
+    { x: -1, y: 0 },
+    { x: 1, y: 0 },
+    { x: -1, y: -1 },
+    { x: 0, y: -1 },
+    { x: 1, y: -1 },
+  ].map(({ x: translationX, y: translationY }) => ({ x: xCell + translationX, y: yCell + translationY }));
+
+const extractAliveNeighbours = (currentCell: AliveCell, grid: Grid): AliveCell[] =>
   grid.filter((cell) => areNeighbours(currentCell, cell));
 
 const isInUnderpopulation = (numberOfNeighbours: number): boolean => {
@@ -21,16 +36,24 @@ const isInOverpopulation = (numberOfNeighbours: number): boolean => {
   return numberOfNeighbours > 3;
 };
 
-export const nextStatus = (numberOfNeighbours: number): "alive" | "dead" => {
+type CellStatus = "alive" | "dead";
+
+export const nextStatus = (numberOfNeighbours: number, currentStatus: CellStatus): "alive" | "dead" => {
+  if (currentStatus === "dead") {
+    return numberOfNeighbours === 3 ? "alive" : "dead";
+  }
+
   return isInUnderpopulation(numberOfNeighbours) || isInOverpopulation(numberOfNeighbours) ? "dead" : "alive";
 };
 
 const shouldStayAlive =
   (grid: Grid) =>
   (cell: AliveCell): boolean => {
-    const neighbours = extractNeighbours(cell, grid);
+    const liveNeighbours = extractAliveNeighbours(cell, grid);
 
-    return nextStatus(neighbours.length) === "alive";
+    const deadNeighbours = allNeighbours(cell).filter((cell) => !liveNeighbours.some(isSameCell(cell)));
+
+    return nextStatus(liveNeighbours.length, "alive") === "alive";
   };
 
 export const computeNextGeneration = (inputCells: Grid): Grid => {
